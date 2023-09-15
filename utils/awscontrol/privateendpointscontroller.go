e5ae3b4603498ef38113bbd6760a919589aab554
Brett Lawson <brett19@gmail.com>
2023-09-14 17:26:45 -0700

    Added a sleep after updating AWS Private DNS to avoid waits.
    If we fetch the record before it is propagated, we end up needing
    to wait for 5 minutes for the DNS cache to invalidate.

--- utils/awscontrol/privateendpointscontroller.go
+++ utils/awscontrol/privateendpointscontroller.go
@@ -158,6 +158,10 @@ func (c *PrivateEndpointsController) EnableVPCEndpointPrivateDNS(ctx context.Con
 		return errors.Wrap(err, "failed to wait for available state")
 	}
 
+	// its safer for us to sleep here for 10s than to accidentally fetch the DNS
+	// entry before its been propagated and then need to wait 5m for it to update.
+	time.Sleep(10 * time.Second)
+
 	return nil
 }
 
