7f9ba22ecec9d8ee3e8821a0783a4ddb404db587
Sergey Avseyev <sergey.avseyev@gmail.com>
2024-05-08 07:41:40 -0700

    JSON output for 'cbdinocluster ps'

--- cmd/list.go
+++ cmd/list.go
@@ -15,6 +15,23 @@ type deployerCluster struct {
 	Info         deployment.ClusterInfo
 }
 
+type ClusterListOutput []ClusterListOutput_Item
+
+type ClusterListOutput_Item struct {
+	ID       string                   `json:"id"`
+	State    string                   `json:"state"`
+	Expiry   *time.Time               `json:"expiry,omitempty"`
+	Deployer string                   `json:"deployer"`
+	Nodes    []ClusterListOutput_Node `json:"nodes"`
+}
+
+type ClusterListOutput_Node struct {
+	ID         string `json:"id"`
+	Name       string `json:"name"`
+	IPAddress  string `json:"ip_address"`
+	ResourceID string `json:"resource_id"`
+}
+
 var listCmd = &cobra.Command{
 	Use:     "list",
 	Aliases: []string{"ls", "ps"},
@@ -23,6 +40,8 @@ var listCmd = &cobra.Command{
 		helper := CmdHelper{}
 		logger := helper.GetLogger()
 		ctx := helper.GetContext()
+
+		outputJson, _ := cmd.Flags().GetBool("json")
 
 		var wg sync.WaitGroup
 		clustersCh := make(chan *deployerCluster, 1024)
@@ -57,29 +76,56 @@ var listCmd = &cobra.Command{
 			clusters = append(clusters, clusterInfo)
 		}
 
-		fmt.Printf("Clusters:\n")
-		for _, clusterInfo := range clusters {
-			deployerName := clusterInfo.DeployerName
-			cluster := clusterInfo.Info
+		if !outputJson {
+			fmt.Printf("Clusters:\n")
+			for _, clusterInfo := range clusters {
+				deployerName := clusterInfo.DeployerName
+				cluster := clusterInfo.Info
 
-			expiry := cluster.GetExpiry()
-			expiryStr := "none"
-			if !expiry.IsZero() {
-				expiryStr = time.Until(cluster.GetExpiry()).Round(time.Second).String()
-			}
+				expiry := cluster.GetExpiry()
+				expiryStr := "none"
+				if !expiry.IsZero() {
+					expiryStr = time.Until(cluster.GetExpiry()).Round(time.Second).String()
+				}
 
-			fmt.Printf("  %s [State: %s, Timeout: %s, Deployer: %s]\n",
-				cluster.GetID(),
-				cluster.GetState(),
-				expiryStr,
-				deployerName)
-			for _, node := range cluster.GetNodes() {
-				fmt.Printf("    %-16s  %-20s %-20s %s\n",
-					node.GetID(),
-					node.GetName(),
-					node.GetIPAddress(),
-					node.GetResourceID())
+				fmt.Printf("  %s [State: %s, Timeout: %s, Deployer: %s]\n",
+					cluster.GetID(),
+					cluster.GetState(),
+					expiryStr,
+					deployerName)
+				for _, node := range cluster.GetNodes() {
+					fmt.Printf("    %-16s  %-20s %-20s %s\n",
+						node.GetID(),
+						node.GetName(),
+						node.GetIPAddress(),
+						node.GetResourceID())
+				}
 			}
+		} else {
+			var out ClusterListOutput
+			for _, cluster := range clusters {
+				clusterItem := ClusterListOutput_Item{
+					ID:       cluster.Info.GetID(),
+					State:    cluster.Info.GetState(),
+					Deployer: cluster.DeployerName,
+				}
+
+				expiry := cluster.Info.GetExpiry()
+				if !expiry.IsZero() {
+					clusterItem.Expiry = &expiry
+				}
+
+				for _, node := range cluster.Info.GetNodes() {
+					clusterItem.Nodes = append(clusterItem.Nodes, ClusterListOutput_Node{
+						ID:         node.GetID(),
+						Name:       node.GetName(),
+						IPAddress:  node.GetIPAddress(),
+						ResourceID: node.GetResourceID(),
+					})
+				}
+				out = append(out, clusterItem)
+			}
+			helper.OutputJson(out)
 		}
 	},
 }
