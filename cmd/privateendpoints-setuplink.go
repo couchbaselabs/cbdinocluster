06aef444d947d84320cda65980e44aaee3a0dcdc
Aaliya7516 <aaliya7516@gmail.com>
2025-06-05 19:32:13 +0530

    SDKQE-3617: Enable PSC linking to GCP instances - enable remote PL setup

--- cmd/privateendpoints-setuplink.go
+++ cmd/privateendpoints-setuplink.go
@@ -1,10 +1,13 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/couchbaselabs/cbdinocluster/deployment/clouddeploy"
 	"github.com/couchbaselabs/cbdinocluster/utils/awscontrol"
 	"github.com/couchbaselabs/cbdinocluster/utils/azurecontrol"
+	"github.com/couchbaselabs/cbdinocluster/utils/capellacontrol"
 	"github.com/couchbaselabs/cbdinocluster/utils/cloudinstancecontrol"
+	"github.com/couchbaselabs/cbdinocluster/utils/gcpcontrol"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 )
@@ -22,7 +25,8 @@ var privateEndpointsSetupLinkCmd = &cobra.Command{
 		shouldAutoConfig, _ := cmd.Flags().GetBool("auto")
 		instanceId, _ := cmd.Flags().GetString("instance-id")
 		vmId, _ := cmd.Flags().GetString("vm-id")
-
+		instanceIdGcp, _ := cmd.Flags().GetString("gcp-instance-id")
+		projectIdGcp, _ := cmd.Flags().GetString("gcp-project-id")
 		_, deployer, cluster := helper.IdentifyCluster(ctx, args[0])
 
 		cloudDeployer, ok := deployer.(*clouddeploy.Deployer)
@@ -33,8 +37,8 @@ var privateEndpointsSetupLinkCmd = &cobra.Command{
 		cloudCluster := cluster.(*clouddeploy.ClusterInfo)
 
 		if shouldAutoConfig {
-			if instanceId != "" || vmId != "" {
-				logger.Fatal("must not specify both auto and instance-id/vm-id")
+			if instanceId != "" || vmId != "" || instanceIdGcp != "" {
+				logger.Fatal("must not specify both auto and instance-id/vm-id/gcp-instance-id")
 			}
 
 			siCtrl := cloudinstancecontrol.SelfIdentifyController{
@@ -51,16 +55,19 @@ var privateEndpointsSetupLinkCmd = &cobra.Command{
 				instanceId = selfIdentity.InstanceID
 			case *azurecontrol.LocalVmInfo:
 				vmId = selfIdentity.VmID
+			case *gcpcontrol.LocalInstanceInfo:
+				instanceIdGcp = selfIdentity.InstanceID
+				projectIdGcp = selfIdentity.ProjectID
 			default:
 				logger.Fatal("unexpected self-identity type")
 			}
 		}
 
-		if instanceId == "" && vmId == "" {
-			logger.Fatal("must specify either auto or instance-id/vm-id")
+		if instanceId == "" && vmId == "" && instanceIdGcp == "" {
+			logger.Fatal("must specify either auto or instance-id/vm-id/gcp-instance-id")
 		}
-		if instanceId != "" && vmId != "" {
-			logger.Fatal("must not specify multiple of instance-id,vm-id")
+		if (instanceId != "" && vmId != "") || (instanceIdGcp != "" && (instanceId != "" || vmId != "")) {
+			logger.Fatal("must not specify multiple of instance-id,vm-id/gcp-instance-id")
 		}
 
 		pe, err := cloudDeployer.GetPrivateEndpointDetails(ctx, cloudCluster.ClusterID)
@@ -142,6 +149,51 @@ var privateEndpointsSetupLinkCmd = &cobra.Command{
 			if err != nil {
 				logger.Fatal("failed to enable private dns", zap.Error(err))
 			}
+		} else if instanceIdGcp != "" {
+			if !config.GCP.Enabled.Value() {
+				logger.Fatal("cannot setup GCP private endpoint without GCP configuration")
+			}
+
+			if projectIdGcp == "" {
+				logger.Fatal("must specify gcp-project-id when using gcp-instance-id")
+			}
+
+			gcpCreds := helper.GetGCPCredentials(ctx)
+
+			peCtrl := gcpcontrol.PrivateEndpointsController{
+				Logger:    logger,
+				Zone:      config.GCP.Zone,
+				Creds:     gcpCreds,
+				ProjectID: projectIdGcp,
+			}
+
+			vpcInfo, err := peCtrl.GetNetworkAndSubnet(ctx, instanceIdGcp)
+			if err != nil {
+				logger.Fatal("failed to get network and subnet for GCP instance", zap.Error(err))
+			}
+
+			command, err := cloudDeployer.GenPrivateEndpointLinkCommand(ctx, cloudCluster.ClusterID, &capellacontrol.PrivateEndpointLinkRequest{
+				VpcID:     vpcInfo.Network,
+				SubnetIds: vpcInfo.Subnetwork,
+			})
+
+			fmt.Print("\n\nCommand: " + command + "\n\n")
+			_, err = peCtrl.CreateVPCEndpoint(ctx, &gcpcontrol.CreateVPCEndpointOptions{
+				ClusterID:  cloudCluster.CloudClusterID,
+				InstanceID: instanceIdGcp,
+			})
+			if err != nil {
+				logger.Fatal("failed to create private service connect endpoint", zap.Error(err))
+			}
+
+			err = cloudDeployer.AcceptPrivateEndpointLink(ctx, cloudCluster.ClusterID, projectIdGcp)
+			if err != nil {
+				logger.Fatal("failed to accept private endpoint link", zap.Error(err))
+			}
+
+			if err != nil {
+				logger.Fatal("failed to enable private dns on link", zap.Error(err))
+			}
 		} else {
 			logger.Fatal("unexpectedly missing instance identifier")
 		}
@@ -153,5 +205,7 @@ func init() {
 
 	privateEndpointsSetupLinkCmd.Flags().String("instance-id", "", "The AWS instance id to setup the link for")
 	privateEndpointsSetupLinkCmd.Flags().String("vm-id", "", "The Azure virtual machine id to setup the link for")
+	privateEndpointsSetupLinkCmd.Flags().String("gcp-instance-id", "", "The GCP instance id to setup the link for")
+	privateEndpointsSetupLinkCmd.Flags().String("gcp-project-id", "", "The GCP project id to use with the gcp-instance-id")
 	privateEndpointsSetupLinkCmd.Flags().Bool("auto", false, "Attempt to identify the local instance")
 }
